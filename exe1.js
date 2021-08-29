899bd00545a08720713b85e172c809baed5803ed
tsoriLache <89573774+tsoriLache@users.noreply.github.com>
2021-08-29 21:00:51 +0300

    some the digits of 3digit number
    i declared 3 varibales one for each digit.
    and then sum them all up

--- exe1.js
+++ exe1.js
@@ -1,1 +1,8 @@
-
+"use strict";
+const num = 123;
+let sum = 0;
+const ones=num%10;
+const tens=(num%100-ones)/10;
+const hundreds=(num-ones-(tens*10))/100;
+sum= ones+tens+hundreds;
+console.log(sum);
