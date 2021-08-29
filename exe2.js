2fb9012eeab0450c74c7e7f7543513013462c245
tsoriLache <89573774+tsoriLache@users.noreply.github.com>
2021-08-29 21:44:24 +0300

    solve quadratic equation.
    solve the quadratic equation .

--- exe2.js
+++ exe2.js
@@ -1,1 +1,6 @@
-
+"use strict"
+const a = 1, b = -6, c = 9;  // stands for x**2 - 6*x + 9 = 0
+let solutionA =0, solutionB = 0
+solutionA=(-b+(Math.sqrt((b*b)-(4*a*c))))/(2*a);
+solutionB=(-b-(Math.sqrt((b*b)-(4*a*c))))/(2*a);
+console.log(solutionA, solutionB);
