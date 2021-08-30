5636dd5d5d58aedf8f89e5cf519b4745825d179f
tsoriLache <89573774+tsoriLache@users.noreply.github.com>
2021-08-30 10:09:30 +0300

    added documentation
    Documentation

--- exe1.js
+++ exe1.js
@@ -1,8 +1,8 @@
 "use strict";
 const num = 123;
 let sum = 0;
-const ones=num%10;
-const tens=(num%100-ones)/10;
-const hundreds=(num-ones-(tens*10))/100;
+const ones=num%10; //מבודד את ספרת האחדות
+const tens=(num%100-ones)/10;// מבודד את ספרט העשרות
+const hundreds=(num-ones-(tens*10))/100; //מבודד את ספרת המאות
 sum= ones+tens+hundreds;
 console.log(sum);
