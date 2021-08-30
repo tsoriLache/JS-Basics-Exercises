9b0bf92ff92ec4b8bf0b9c632dab5e77b542e563
tsoriLache <89573774+tsoriLache@users.noreply.github.com>
2021-08-30 18:19:20 +0300

    grades statistics
    done

--- grades statistics.js
+++ grades statistics.js
@@ -1,1 +1,13 @@
-
+"use strict"
+const grades = [0,0,0,0,0,0,0,0,0,0,0,0,0,0,73, 91, 88, 56, 62, 84, 95, 96, 88, 92,100,120,34,75];
+const secondHalfGrades=[...grades.slice(Math.floor(grades.length/2))];
+function rangeOfGrades(grades)
+{
+    return Math.max(...grades)-Math.min(...grades);
+}
+function medianGrade(grades)
+{
+    grades.sort(function(a, b){return a-b}); //מסדר את הציונים במערך מהקטן לגדול
+    return grades[Math.floor(grades.length/2)]
+}
+console.log("Stats:"+"\n\t"+"Range: "+rangeOfGrades(grades)+"\n\t"+"Median: "+medianGrade(grades)+"\n\t"+"Half Range: "+rangeOfGrades(secondHalfGrades));
