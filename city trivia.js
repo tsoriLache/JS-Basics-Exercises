573bc535444099c4c6d401ce70d1934083729fb5
tsoriLache <89573774+tsoriLache@users.noreply.github.com>
2021-08-31 20:44:13 +0300

    Update city trivia.js
    finished,i'm not sure if that is how gabi ment for us to solve.

--- city trivia.js
+++ city trivia.js
@@ -1,1 +1,10 @@
+"use strict"
+function guessCity(capital, coastal, famous, ancient) {
+    if(capital&&famous&&ancient&&!coastal)return "Jerusalem";
+    if(!capital&&famous&&!ancient&&coastal)return "Tel Aviv";
+    if(!capital&&!famous&&ancient&&coastal)return "Acre";
+    if(!capital&&!famous&&ancient&&!coastal)return"Katzrin";
+    if(!capital&&!famous&&!ancient&&!coastal) return "Musmus";
+}
+console.log(guessCity(false, true, false, true));
 
