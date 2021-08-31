66a3ab70458fbd3e00681551dc9c9a2e5d26d1ab
tsoriLache <89573774+tsoriLache@users.noreply.github.com>
2021-08-31 21:08:55 +0300

    Update Formula1.js
    simple condition.

--- Formula1.js
+++ Formula1.js
@@ -1,1 +1,38 @@
+"use strict"
+// list of Formula 1 champions since the year 2000
+const formula1Champions = [
+    "Schumacher",
+    "Schumacher",
+    "Schumacher",
+    "Schumacher",
+    "Schumacher",
+    "Alonso",
+    "Alonso",
+    "Räikkönen",
+    "Hamilton",
+    "Button",
+    "Vettel",
+    "Vettel",
+    "Vettel",
+    "Vettel",
+    "Hamilton",
+    "Hamilton",
+    "Rosberg",
+    "Hamilton",
+    "Hamilton",
+    "Hamilton",
+    "Hamilton"
+]
+let winners = {}
 
+function countWin(driver) {
+    if(winners.hasOwnProperty(driver)){
+        winners[driver]++;
+    }
+    else{
+        winners[driver]=1;
+    }
+}
+
+formula1Champions.forEach(countWin);
+    
